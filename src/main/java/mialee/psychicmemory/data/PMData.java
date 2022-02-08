e1f39c85c6a7f82be915c6cf1eb399c97f550915
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2022-02-08 22:50:46 +0000

    Update PMData.java

--- src/main/java/mialee/psychicmemory/data/PMData.java
+++ src/main/java/mialee/psychicmemory/data/PMData.java
@@ -16,8 +16,14 @@ public class PMData {
     //If no json is present generates a new one and saves it.
     public static PMSettings populateSettings() {
         PMSettings pmSettings = new PMSettings();
-        PMData.load(Path.of("%s/settings.json".formatted(dir)), pmSettings);
+
+        //If the file isn't loaded, print that it will generate a new one.
+        boolean loaded = PMData.load(Path.of("%s/settings.json".formatted(dir)), pmSettings);
+        if (!loaded) System.out.print("Generating new settings file.\n");
+
+        //Saves file immediately, easier to edit if needed.
         PMData.save(Path.of("%s/settings.json".formatted(dir)), pmSettings);
+
         return pmSettings;
     }
 
@@ -25,13 +31,19 @@ public class PMData {
     //If no save is present generates a new one and saves it.
     public static PMSave populateSave(int save) {
         PMSave pmSave = new PMSave();
-        PMData.load(Path.of("%s/save%d.json".formatted(dir, save)), pmSave);
+
+        //If the file isn't loaded, print that it will generate a new one.
+        boolean loaded = PMData.load(Path.of("%s/save%d.json".formatted(dir, save)), pmSave);
+        if (!loaded) System.out.printf("Generating new save file in save slot %d.\n", save);
+
+        //Saves file immediately, easier to edit if needed.
         PMData.save(Path.of("%s/save%d.json".formatted(dir, save)), pmSave);
+
         return pmSave;
     }
 
     //Loads data from a selected json file to the selected Object.
-    private static void load(Path path, Object map) {
+    public static boolean load(Path path, Object map) {
         try {
             //Reads data from the existing json file.
             //If file is not present, throws an exception and exits the method safely with the try, catch.
@@ -45,26 +57,52 @@ public class PMData {
                     if (jsonObject.has(field.getName())) {
                         map.getClass().getField(field.getName()).set(map, jsonObject.get(field.getName()));
                     }
-                } catch (Exception ignored) {}
+                } catch (Exception ignored) {
+
+                }
             }
-        } catch (IOException ignored) {}
+
+            //Returns true if file was loaded successfully.
+            System.out.printf("%s read successfully.\n", path.getName(path.getNameCount() - 1));
+            return true;
+        } catch (IOException e) {
+            //Returns false if file not found and sends to log.
+            System.out.printf("File %s not found.\n", path.getName(path.getNameCount() - 1));
+            return false;
+        }
     }
 
     //Saves data from an object to a selected json file at the path.
-    private static void save(Path path, Object map) {
+    public static void save(Path path, Object map) {
         JSONObject jsonObject = new JSONObject();
 
         //Takes data from all fields in the Object and writes them to the json object.
         for (Field field : map.getClass().getFields()) {
             try {
                 jsonObject.put(field.getName(), map.getClass().getField(field.getName()).get(map));
-            } catch (Exception ignored) {}
+            } catch (NoSuchFieldException e) {
+                //This won't occur as fields are taken from the class.
+                System.out.printf("Unexpected error.\n%s\n", e.getMessage());
+            } catch (IllegalAccessException e) {
+                System.out.printf("Unable to save %s classes to files as they are inaccessible.\n%s\n", map.getClass().getName(), e.getMessage());
+            }
         }
 
         //Writes the data to a new json file at given path.
         try (FileWriter jsonFile = new FileWriter(String.valueOf(path))) {
             jsonFile.write(jsonObject.toString());
             jsonFile.flush();
-        } catch (IOException ignored) {}
+        } catch (IOException e) {
+            System.out.printf("Failed to save data.\n%s\n", e.getMessage());
+        }
+    }
+
+    //Deletes data from a json returning it to its original values.
+    public static void wipe(Path path, Object map) {
+        try {
+            save(path, map.getClass().newInstance());
+        } catch (InstantiationException | IllegalAccessException e) {
+            System.out.printf("Failed to wipe save data.\n%s\n", e.getMessage());
+        }
     }
 }
